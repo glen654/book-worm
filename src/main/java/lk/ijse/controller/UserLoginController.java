9694c100468281e9f6531262bc4cfbe1aa25e998
Glen Alloy <glenalloy8@gmail.com>
2024-02-29 22:19:53 +0530

    user login ui done

---
+++ src/main/java/lk/ijse/controller/UserLoginController.java
@@ -0,0 +1,31 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class UserLoginController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpChangeOnAction(ActionEvent event) {
+
+    }
+}
