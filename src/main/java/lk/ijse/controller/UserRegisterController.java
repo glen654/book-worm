db18fa4353e8692f736a3e521fb34b69d5846266
Glen Alloy <glenalloy8@gmail.com>
2024-02-29 22:06:22 +0530

    user register ui done

---
+++ src/main/java/lk/ijse/controller/UserRegisterController.java
@@ -0,0 +1,34 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class UserRegisterController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private PasswordField txtConfirmPassword;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnSIgnInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSIgnUpAction(ActionEvent event) {
+
+    }
+}
