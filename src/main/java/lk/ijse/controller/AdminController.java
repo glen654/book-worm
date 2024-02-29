0d7b0a834fb422b617ef2dd9adb073bc57e0f5d4
Glen Alloy <glenalloy8@gmail.com>
2024-02-29 21:42:56 +0530

    admin login and register ui done

---
+++ src/main/java/lk/ijse/controller/AdminController.java
@@ -0,0 +1,36 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private PasswordField txtConfirmPassword;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+
+    @FXML
+    void btnSIgnInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSIgnUpAction(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/lk/ijse/controller/AdminLoginController.java
@@ -0,0 +1,33 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminLoginController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpChangeOnAction(ActionEvent event) {
+
+    }
+}
