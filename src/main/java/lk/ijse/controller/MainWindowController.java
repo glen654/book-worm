99f3903cc63a046b44caf63d32270bf6f50ccd7c
Glen Alloy <glenalloy8@gmail.com>
2024-03-01 19:33:08 +0530

    few changes

--- src/main/java/lk/ijse/controller/MainWindowController.java
+++ src/main/java/lk/ijse/controller/MainWindowController.java
@@ -2,7 +2,13 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainWindowController {
     @FXML
@@ -12,13 +18,25 @@ public class MainWindowController {
     private AnchorPane rootNode;
 
     @FXML
-    void adminOnAction(ActionEvent event) {
+    void adminOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/admin_login.fxml"));
 
+        Scene scene = new Scene(rootNode);
+
+        Stage primaryStage =(Stage) this.rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Book Worm");
     }
 
     @FXML
-    void userOnAction(ActionEvent event) {
+    void userOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/user_login.fxml"));
 
+        Scene scene = new Scene(rootNode);
+
+        Stage primaryStage =(Stage) this.rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Book Worm");
     }
 
 }
