7dba9b31da0c903d427226d8c042acff96f8dbd7
Glen Alloy <glenalloy8@gmail.com>
2024-03-01 07:54:45 +0530

    main ui done

---
+++ src/main/java/lk/ijse/controller/MainWindowController.java
@@ -0,0 +1,24 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MainWindowController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    void adminOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userOnAction(ActionEvent event) {
+
+    }
+
+}
