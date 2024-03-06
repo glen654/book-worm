0b8d299ceaf474d94245cc61aff6c76d783ed0d7
Glen Alloy <glenalloy8@gmail.com>
2024-03-06 22:09:46 +0530

    few changes

--- src/main/java/lk/ijse/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/config/FactoryConfiguration.java
@@ -2,6 +2,7 @@ package lk.ijse.config;
 
 import lk.ijse.entity.Admin;
 import lk.ijse.entity.Book;
+import lk.ijse.entity.Branch;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -27,7 +28,7 @@ public class FactoryConfiguration {
             throw new RuntimeException(e);
         }
 
-        Configuration configuration = (new Configuration()).addProperties(properties).addAnnotatedClass(Admin.class).addAnnotatedClass(Book.class);
+        Configuration configuration = (new Configuration()).addProperties(properties).addAnnotatedClass(Admin.class).addAnnotatedClass(Book.class).addAnnotatedClass(Branch.class);
         this.sessionFactory = configuration.buildSessionFactory();
     }
 
