4513bb201a657846fa1f303c6f56b2197f466b48
Glen Alloy <glenalloy8@gmail.com>
2024-03-10 12:07:48 +0530

    user dashboard done

---
+++ src/main/java/lk/ijse/controller/MainUserController.java
@@ -0,0 +1,43 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class MainUserController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private Label txtBorrowedBooks;
+
+    @FXML
+    private Label txtDueBooks;
+
+    @FXML
+    private Label txtName;
+
+    @FXML
+    void btnBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignOutOnAction(ActionEvent event) {
+
+    }
+}
