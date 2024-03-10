62d53ed18ab6f9066885b8c61f1b112b9ee6823d
Glen Alloy <glenalloy8@gmail.com>
2024-03-10 10:56:00 +0530

    user entity and id generator done

---
+++ src/main/java/lk/ijse/entity/User.java
@@ -0,0 +1,69 @@
+package lk.ijse.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(generator = "User-Id-Generator")
+    @GenericGenerator(name = "User-Id-Generator", strategy = "lk.ijse.util.UserIdGenerator")
+    private String uId;
+    private String userName;
+    private String password;
+    private String confirmPassword;
+
+    public User() {
+    }
+
+    public User(String uId, String userName, String password, String confirmPassword) {
+        this.uId = uId;
+        this.userName = userName;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uId='" + uId + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", confirmPassword='" + confirmPassword + '\'' +
+                '}';
+    }
+}
