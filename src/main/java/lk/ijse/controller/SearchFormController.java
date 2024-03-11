e03dd740bed216958240c81b6fc67d7d20882453
Glen Alloy <glenalloy8@gmail.com>
2024-03-11 11:26:24 +0530

    search form done

---
+++ src/main/java/lk/ijse/controller/SearchFormController.java
@@ -0,0 +1,31 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class SearchFormController {
+    @FXML
+    private Label lblAuthor;
+
+    @FXML
+    private Label lblBookId;
+
+    @FXML
+    private Label lblBookName;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    void btnBorrowOnAction(ActionEvent event) {
+
+    }
+}
--- src/main/java/lk/ijse/controller/UserBookController.java
+++ src/main/java/lk/ijse/controller/UserBookController.java
@@ -2,9 +2,15 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserBookController {
     @FXML
@@ -25,8 +31,14 @@ public class UserBookController {
     }
 
     @FXML
-    void btnDashboardOnAction(ActionEvent event) {
+    void btnDashboardOnAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/user_dashboard.fxml"));
 
+        Scene scene = new Scene(root);
+
+        Stage primaryStage =(Stage) this.rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Book Worm");
     }
 
     @FXML
@@ -40,8 +52,14 @@ public class UserBookController {
     }
 
     @FXML
-    void btnSignOutOnAction(ActionEvent event) {
+    void btnSignOutOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/user_login.fxml"));
 
+        Scene scene = new Scene(rootNode);
+
+        Stage primaryStage =(Stage) this.rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Book Worm");
     }
 
 }
