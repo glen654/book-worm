3a929776d6cd82e9a3d03cecc05ff7f905ed2438
Glen Alloy <glenalloy8@gmail.com>
2024-03-11 11:07:20 +0530

    book search ui done

---
+++ src/main/java/lk/ijse/controller/UserBookController.java
@@ -0,0 +1,47 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class UserBookController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private Label txtName;
+
+    @FXML
+    private TextField txtSearchBook;
+
+    @FXML
+    void btnBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignOutOnAction(ActionEvent event) {
+
+    }
+
+}
