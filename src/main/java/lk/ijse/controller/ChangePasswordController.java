04e7653aaa420e0d1f0e474ede591600cd2181ab
Glen Alloy <glenalloy8@gmail.com>
2024-03-11 20:08:06 +0530

    user change password ui done

---
+++ src/main/java/lk/ijse/controller/ChangePasswordController.java
@@ -0,0 +1,52 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ChangePasswordController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private TextField txtConfirmPassword;
+
+    @FXML
+    private Label txtName;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdatePasswordOnAction(ActionEvent event) {
+
+    }
+}
